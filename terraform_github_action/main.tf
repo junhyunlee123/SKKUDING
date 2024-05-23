0cd5c19ec1b54124ece952bade0ad27a078386ac
junhyunlee123 <tedjoon@icloud.com>
2024-05-24 00:46:48 +0900

    replaced manual docker build with action

--- terraform_github_action/main.tf
+++ terraform_github_action/main.tf
@@ -106,7 +106,7 @@ resource "aws_cloudfront_distribution" "s3_expressjs_distribution" {
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "terraform implementation of s3 and cloudfront part of nginx_expressjs project"
+  comment             = "terraform implementation of s3 and cloudfront part of nginx_expressjs project using Github Action"
   default_root_object = "index.html"
 
   default_cache_behavior {
